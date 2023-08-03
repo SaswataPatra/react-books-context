bbd051d9947b4d87dd79d850c928060b88b0d1da
PoMaMa69 <saswatapatra15@gmail.com>
2023-08-03 23:34:16 +0530

    all about providers done

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -3,8 +3,7 @@ import BookEdit from "./BookEdit";
 import { useContext } from "react";
 import BooksContext from "../context/Books";
 function BookShow({ book, onDelete, onEdit }) {
-  const value = useContext(BooksContext);
-
+  const {count, incrementCounter} = useContext(BooksContext);
 
   const [showEdit, setShowEdit] = useState(false);
 
@@ -25,11 +24,13 @@ function BookShow({ book, onDelete, onEdit }) {
   }
   return (
     <div className="book-show">
+      {count} {" "}
+      <button onClick={incrementCounter}>click me!</button>
       <img
         alt="books"
         src={`https://picsum.photos/seed/${book.id}300/200`}
       ></img>
-      {value}
+
       <br></br>
       {content}
       <div className="actions">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
-import BooksContext from "./context/Books";
+import { Provider } from "./context/Books";
 
 import "./index.css";
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
 
 root.render(
-  <BooksContext.Provider value={5}>
+  <Provider>
     <App />
-  </BooksContext.Provider>
+  </Provider>
 );
