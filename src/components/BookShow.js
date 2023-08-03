1dd567bd4b728fd741f346655ce61490e85a7fc9
PoMaMa69 <saswatapatra15@gmail.com>
2023-08-03 23:10:24 +0530

    hardcoded provider

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,31 +1,45 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
-function BookShow({ book, onDelete,onEdit}) {
-    const [showEdit,setShowEdit] = useState(false);
+import { useContext } from "react";
+import BooksContext from "../context/Books";
+function BookShow({ book, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
 
 
-    const handleDeleteClick = () =>{
-        onDelete(book.id);
-    }
-    const handleEditClick = () =>{
-        setShowEdit(!showEdit);
-    }
-    const handSubmit =(id,newTitle) =>{
-        setShowEdit(false);
-        onEdit(id,newTitle);
-    }
+  const [showEdit, setShowEdit] = useState(false);
 
-    let content = book.title;
-    if(showEdit){
-        content = <BookEdit onSubmit={handSubmit} book={book}/>;
-    }
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+  const handSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = book.title;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handSubmit} book={book} />;
+  }
   return (
     <div className="book-show">
-    <img alt="books" src={`https://picsum.photos/seed/${book.id}300/200`}></img>
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}300/200`}
+      ></img>
+      {value}
+      <br></br>
       {content}
       <div className="actions">
-      <button className="edit" onClick={handleEditClick} > Edit</button>
-        <button className="delete" onClick={handleDeleteClick}>Delete</button>
+        <button className="edit" onClick={handleEditClick}>
+          {" "}
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
-import App from './App';
-import './index.css';
-const el = document.getElementById('root');
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+import BooksContext from "./context/Books";
+
+import "./index.css";
+const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
 
-root.render(<App/>);
-
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
